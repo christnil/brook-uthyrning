5035da81eb68ae59eb95d7e68b3a005499d0a40e
Christoffer Nilsson <christoffer.nilsson@svenskaspel.se>
2025-02-02 21:05:33 +0100

    Comment out navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 
-import Navbar from '@/components/navbar';
+//import Navbar from '@/components/navbar';
 
 import "./globals.css";
 
